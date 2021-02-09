9d036e3d64ffba889f7e4ec345adf762afb1fd13
RainierEH <swc.rainier@gmail.com>
2021-02-08 16:30:44 -0800

    Adding the fucking database module and using it's test function, taken from the golang website. btw: FUCK

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	db "./src/database"
 	"fmt"
 )
 
 func main() {
 	fmt.Println("Test?")
+	teststr := db.Test("Test!")
+	fmt.Println(teststr)
 }
---
+++ src/database/database.go
@@ -0,0 +1,10 @@
+package database
+
+import (
+	"fmt"
+)
+
+func Test(input string) string {
+	val := fmt.Sprintf("%v", input)
+	return val
+}
