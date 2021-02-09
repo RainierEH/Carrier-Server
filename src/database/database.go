dcd2f1ca8e932dab9746c6e8492e50796cb4d427
RainierEH <swc.rainier@gmail.com>
2021-02-08 16:58:01 -0800

    FUCKING KILL ME CHRIST I HATE THIS GOD DAMN PROJECT JUST END MY SUFFERING (I'm still fucking trying to import mysql drivers)

--- src/database/database.go
+++ src/database/database.go
@@ -1,10 +1,17 @@
 package database
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 )
+_
+func init() {
+	sql.Register("sql", &MySQLDriver{})
+}
 
 func Test(input string) string {
 	val := fmt.Sprintf("%v", input)
 	return val
 }
+
